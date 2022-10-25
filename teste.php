1a98f2d1d105961141f8dec1bdcddc9e093ae6b3
iur1dev <luric.dev@gmail.com>
2022-10-25 14:06:18 -0300

    teste2

--- teste.php
+++ teste.php
@@ -1,6 +1,26 @@
 <?php
+
+$host = 'localhost';
+$login = "root";
+$senha = '';
+$bd = "barber";
+$conn = mysqli_connect($host, $login, $senha, $bd);
+
 $agora = new DateTime();
 $periodo = new DateInterval('P1D');
+
+$sqlSelect = "SELECT * FROM teste";
+$query1 = $conn->query($sqlSelect) or die("404 sql" . $conn->error);
+
+
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+    $campo1 = $_POST['campo1'];
+    $campo2 = $_POST['campo2'];
+    $campo3 = $_POST['campo3'];
+
+    $sqlInsert = "INSERT INTO teste(campo1, campo2, campo3) VALUES ('$campo1', '$campo2', '$campo3')";
+    $query = $conn->query($sqlInsert) or die("404 sql" . $conn->error);
+}
 
 ?>
 <!DOCTYPE html>
@@ -12,6 +32,7 @@ $periodo = new DateInterval('P1D');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Teste</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="/dist/output.css" rel="stylesheet">
 </head>
 
 <body>
@@ -27,7 +48,7 @@ $periodo = new DateInterval('P1D');
                         <?php
                         for ($i = 1; $i <= 7; $i++) {
                             echo "<option>";
-                            echo $agora->format('d/m/Y');
+                            echo $agora->format('l - d/m/Y');
                             echo "</option>";
                             $agora->add($periodo);
                         }
@@ -45,6 +66,28 @@ $periodo = new DateInterval('P1D');
                 ?>
 
                 <button class="btn btn-primary" type="submit">Enviar</button>
+
+                <form method="POST">
+
+                    <?php
+                    while ($linha = mysqli_fetch_row($query1)) {
+                    ?>
+                        <input type="text" class="form-control my-3" value="<?php if (isset($linha[1])) {
+                                                                                echo $linha[0];
+                                                                            } ?>" placeholder="1" name="campo1">
+                        <input type="text" class="form-control my-3" value="<?php if (isset($linha[1])) {
+                                                                                echo $linha[1];
+                                                                            } ?>" placeholder="2" name="campo2">
+                        <input type="text" class="form-control my-3" value="<?php if (isset($linha[2])) {
+                                                                                echo $linha[2];
+                                                                            } ?>" placeholder="3" name="campo3">
+                    <?php
+                    }
+                    ?>
+
+
+                    <button class="btn btn-primary" type="submit">Enviar</button>
+                </form>
             </div>
         </div>
     </main>
@@ -52,6 +95,7 @@ $periodo = new DateInterval('P1D');
     </footer>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
